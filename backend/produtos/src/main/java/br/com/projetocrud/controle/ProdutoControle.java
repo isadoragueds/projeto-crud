7a8a147425a52452ea0f9a666c0ed97ae7fd12de
isadora <guedessisadora@gmail.com>
2025-03-10 11:13:53 -0300

    Criação do metodo listar

---
+++ backend/produtos/src/main/java/br/com/projetocrud/controle/ProdutoControle.java
@@ -0,0 +1,25 @@
+package br.com.anm.produtos.crud_produtos.controle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.anm.produtos.crud_produtos.modelo.ProdutoModelo;
+import br.com.anm.produtos.crud_produtos.servico.ProdutoServico;
+
+@RestController
+public class ProdutoControle {
+
+    @Autowired
+    private ProdutoServico ps;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar(){
+        return ps.listar();
+    }
+
+    @GetMapping("/")
+    public String rota(){
+        return "A API está funcionando!";
+    }
+}
